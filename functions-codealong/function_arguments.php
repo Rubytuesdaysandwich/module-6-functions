d0e2a83174d84559f19579e8f56eb6421cd114dd
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-26 13:13:13 -0600

    comments

--- functions-codealong/function_arguments.php
+++ functions-codealong/function_arguments.php
@@ -8,19 +8,22 @@
     function say_hello_to($word){
         echo "Hello {$word}!<br/>";//define say_hello_to
     }
-    $name="John Doe";//taking place of $word in the say_hello_to function
+    $name="John Doe";//taking place of $word in the say_hello_to function defining variable $name to John Doe
     say_hello_to($name);
     ?>
 
     <?php
-    function better_hello($greeting, $target, $punct){
+    function better_hello($greeting, $target, $punct){//Defining better hello function with 3 arguments
         echo $greeting . " " . $target . $punct . "<br />";
     }
     
-    better_hello("Hello",$name,"!");
-    better_hello("Greetings",$name,"!!!");
+    better_hello("Hello",$name,"!");//call better hello function
+    better_hello("Greetings",$name,"!!!");//call better hello function
 
-    better_hello("Greetings",$name, null);
+    better_hello("Greetings",$name, null);//call better hello function
+    ?>
+    <?php
+    //!using function arguments we can create our own arguments similar to what php has pre defined and give them our own rules to accomplish tasks in a block of code.
     ?>
   </body>
 </html>
