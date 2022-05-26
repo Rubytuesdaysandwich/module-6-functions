195ca0c8ca3d5ad95213339682d99391493693a8
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-26 12:58:58 -0600

    better hello function

--- functions-codealong/function_arguments.php
+++ functions-codealong/function_arguments.php
@@ -8,10 +8,15 @@
     function say_hello_to($word){
         echo "Hello {$word}!<br/>";//define say_hello_to
     }
-    $name="John Doe";
+    $name="John Doe";//taking place of $word in the say_hello_to function
     say_hello_to($name);
-
-
+    ?>
+    
+    <?php
+    function better_hello($greetings, $tartget, $punct){
+    }
+    
+    
     ?>
   </body>
 </html>
