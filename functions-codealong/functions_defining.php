30d4e322273740739d34530128330b6345e483e7
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-26 12:21:55 -0600

    define and call functions

--- functions-codealong/functions_defining.php
+++ functions-codealong/functions_defining.php
@@ -8,15 +8,21 @@
 function say_hello(){
   echo "Hello World! <br/>";
 }
-say_hello();
+say_hello();//calling say hello function
 
+function say_hello_to($word){
+  echo "Hello {$word}!<br/>";
+}
+say_hello_to("World");
+say_hello_to("Everyone");
 
+say_hello_loudly(){
+  echo "HELLO WORLD!<br/>"
+}
 
-
-
-
-
-
+// say_hello_loudly(){
+//   echo "We can't redefine a function.";
+// }
 ?>
 
 
