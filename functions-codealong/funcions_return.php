24b46ca43302d125f837012348d4497cafca062b
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-02 18:39:45 -0600

    return functions

---
+++ functions-codealong/funcions_return.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Functions: Return Values</title>
+  </head>
+  <body>
+
+<?php
+function add ($val1,$val2) {
+    $sum= $val1 + $val2;
+}
+
+?>
+
+
+
+
+  </body>
+</html>
